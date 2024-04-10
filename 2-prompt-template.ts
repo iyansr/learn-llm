82d05670c2c774453bbf255115c5e2363d55a468
IyanSR <ikhyan7@gmail.com>
2024-04-10 20:25:37 +0800

    prompt template

--- 2-prompt-template.ts
+++ 2-prompt-template.ts
@@ -1,4 +1,5 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "langchain/prompts";
 
 const model = new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
@@ -6,6 +7,29 @@ const model = new ChatOpenAI({
   openAIApiKey: process.env.OPEN_AI_KEY,
 });
 
-const response = await model.invoke("Hello, who are you");
+// const template = ChatPromptTemplate.fromTemplate(
+//   "You are a helpful assistant that translates {input_language} to {output_language}. Response must be in {output_language}. Input: {text}"
+// );
+
+const template = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant that translates {input_language} to {output_language} based on user input"],
+  ["human", "{text}"],
+]);
+
+const prompt = await template.format({
+  input_language: "English",
+  output_language: "French",
+  text: "Hello, who are you?",
+});
+
+console.log(prompt);
+
+const chain = template.pipe(model);
+
+const response = await chain.invoke({
+  input_language: "English",
+  output_language: "French",
+  text: "Hello, who are you?",
+});
 
 console.log(response.content);
